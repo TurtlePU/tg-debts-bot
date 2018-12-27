1f938ca713ceb1ac5411386de859933b923b261a
Paul Sokolov Jr <sokolov.p64@gmail.com>
2018-12-27 06:33:14 +0300

    +bot.js

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
+const fs = require('fs');
+
 const RETRY_ATTEMPTS = 1;
-const retry = (count) => require('fs').readFile('private/token', startBot(count));
+const retry = (count) => fs.readFile('private/token', startBot(count));
 
 const startBot = (count) => (err, token) => {
     if (err) {
-        console.log('Failed to read from file:');
+        console.log('Failed to read from file. Error:');
         console.log(err);
+
         if (count) {
             retry(count - 1);
         } else {
             console.log('No attempts left');
             throw err;
         }
-    }
+    } else {
+        console.log('Successfully read from file. Token:');
+        console.log(token);
 
-    const Bot = require('lib/bot.js')(require('node-telegram-bot-api'));
-    var bot = new Bot(token);
+        const Bot = require('lib/bot.js')(require('node-telegram-bot-api'));
+        var bot = new Bot(token);
+    }
 }
 
 retry(RETRY_ATTEMPTS);
