091280b77b86cd840f3fe137c901176c6b4cfccd
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-01-20 19:20:47 +0300

    moved token to environment variables

--- server.js
+++ server.js
@@ -1,50 +1,26 @@
-const fs = require('fs');
 const http = require('http');
 
-const RETRY_ATTEMPTS = 1;
-const retry = (count) => fs.readFile('private/token', startBot(count));
+const server = `https://tg-debts-bot.herokuapp.com:443`;
 
-const startBot = (count) => (err, token) => {
-    if (err) {
-        console.log('Failed to read from file. Error:');
-        console.log(err);
+const token  = process.env.TG_BOT_TOKEN;
+console.log('TG_BOT_TOKEN:', token);
 
-        if (count) {
-            retry(count - 1);
-        } else {
-            console.log('No attempts left');
-            throw err;
-        }
-    } else {
-        console.log('Successfully read from file. Token:');
-        console.log(token);
+const Bot = require('./lib/bot')(require('node-telegram-bot-api'));
+var bot = new Bot(token, server);
 
-        const server = `https://tg-debts-bot.herokuapp.com:443`;
+const db_url = process.env.DATABASE_URL;
+console.log('DATABASE_URL:', db_url);
 
-        const Bot = require('./lib/bot')(require('node-telegram-bot-api'));
-        var bot = new Bot(token, server);
+const Client = require('./lib/db-wrapper')(require('pg').Client);
+Client.makeWrapper(db_url)
+    .then(client => {
+        bot.setDataBase(client);
 
-        let db_url = process.env.DATABASE_URL;
-
-        console.log('DATABASE_URL:');
-        console.log(db_url);
-
-        const WrapperFactory = require('./lib/db-wrapper');
-        const Client = new WrapperFactory(require('pg').Client);
-
-        Client.makeWrapper(db_url)
-        .then(
-            client => {
-                bot.setDataBase(client);
-
-                // keeps server awake
-                setInterval(() => {
-                    http.get(server);
-                }, 30 * 60 * 1000);  // every 30 minutes
-            },
-            error => console.log(error)
-        );
-    }
-}
-
-retry(RETRY_ATTEMPTS);
+        // keeps server awake
+        setInterval(() => {
+            http.get(server);
+        }, 30 * 60 * 1000);  // every 30 minutes
+    })
+    .catch(error => {
+        console.log(error);
+    });
