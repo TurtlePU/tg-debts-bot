8997a7e3be5405f5cf88ad5377e2fb57478c7dcd
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-01-20 21:54:05 +0300

    hot fix

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ const token  = process.env.TG_BOT_TOKEN;
 const url    = process.env.APP_URL;
 const port   = process.env.PORT;
 const db_url = process.env.DATABASE_URL;
+const name   = process.env.BOT_NAME;
 
 console.log('TG_BOT_TOKEN:', token);
 console.log('APP_URL:', url);
 console.log('PORT:', port);
 console.log('DATABASE_URL:', db_url);
+console.log('BOT_NAME:', name);
 
 const Client = new DBCliFactory(PostGre.Client);
 Client.makeWrapper(db_url)
@@ -22,6 +24,7 @@ Client.makeWrapper(db_url)
         var bot = new Bot(token, 'https://' + url, port);
 
         bot.setDataBase(client);
+        bot.setName(name);
 
         // keeps server awake
         setInterval(() => {
