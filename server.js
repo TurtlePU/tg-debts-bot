9813175296310bf6f159869e194602ac9ad0bceb
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-01-27 17:23:51 +0300

    codestyle

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 const http = require('http');
 
-const Bot      = require('./lib/bot');
-const DBClient = require('./lib/db-wrapper');
-const Cipher   = require('./lib/cipher');
+const Bot      = require('./lib/bot'),
+      DBClient = require('./lib/db-wrapper'),
+      Cipher   = require('./lib/cipher');
 
-const token  = process.env.TG_BOT_TOKEN;
-const url    = process.env.APP_URL;
-const port   = process.env.PORT;
-const db_url = process.env.DATABASE_URL;
-const name   = process.env.BOT_NAME;
+const token  = process.env.TG_BOT_TOKEN,
+      url    = process.env.APP_URL,
+      port   = process.env.PORT,
+      db_url = process.env.DATABASE_URL,
+      name   = process.env.BOT_NAME;
 
-const cipher_key = process.env.CIPHER_KEY;
-const cipher_iv  = process.env.CIPHER_IV;
+const cipher_key = process.env.CIPHER_KEY,
+      cipher_iv  = process.env.CIPHER_IV;
 
-console.log('TG_BOT_TOKEN:', token);
-console.log('APP_URL     :', url);
-console.log('PORT        :', port);
-console.log('DATABASE_URL:', db_url);
-console.log('BOT_NAME    :', name);
-
-console.log('CIPHER_KEY  :', cipher_key);
-console.log('CIPHER_IV   :', cipher_iv);
+console.log(
+    '\nTG_BOT_TOKEN :', token,
+    '\nAPP_URL      :', url,
+    '\nPORT         :', port,
+    '\nDATABASE_URL :', db_url,
+    '\nBOT_NAME     :', name,
+    '\nCIPHER_KEY   :', cipher_key,
+    '\nCIPHER_IV    :', cipher_iv,
+    '\n'
+);
 
 const client = new DBClient(db_url);
 client.start()
@@ -31,14 +33,14 @@ client.start()
         bot.setDataBase(client);
 
         bot.setCipher(new Cipher({
-            key: cipher_key,
-            iv : cipher_iv
+            key : cipher_key,
+            iv  : cipher_iv
         }));
 
         bot.start();
     })
     .catch(error => {
-        console.log(error);
+        console.log('\n', error, '\n');
     });
 
 // keeps server awake
