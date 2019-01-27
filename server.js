e4d93d01da8872ae7e6d8bec3b39d503962685e9
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-01-27 16:33:26 +0300

    db verification -> crypto; codestyle

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const http    = require('http');
-const BaseBot = require('node-telegram-bot-api');
-const PostGre = require('pg');
+const http = require('http');
 
-const BotFactory   = require('./lib/bot');
-const DBCliFactory = require('./lib/db-wrapper');
+const Bot      = require('./lib/bot');
+const DBClient = require('./lib/db-wrapper');
+const Cipher   = require('./lib/cipher');
 
 const token  = process.env.TG_BOT_TOKEN;
 const url    = process.env.APP_URL;
@@ -11,26 +10,38 @@ const port   = process.env.PORT;
 const db_url = process.env.DATABASE_URL;
 const name   = process.env.BOT_NAME;
 
+const cipher_key = process.env.CIPHER_KEY;
+const cipher_iv  = process.env.CIPHER_IV;
+
 console.log('TG_BOT_TOKEN:', token);
-console.log('APP_URL:', url);
-console.log('PORT:', port);
+console.log('APP_URL     :', url);
+console.log('PORT        :', port);
 console.log('DATABASE_URL:', db_url);
-console.log('BOT_NAME:', name);
+console.log('BOT_NAME    :', name);
 
-const Client = new DBCliFactory(PostGre.Client);
-Client.makeWrapper(db_url)
-    .then(client => {
-        const Bot = BotFactory(BaseBot);
-        var bot = new Bot(token, 'https://' + url, port);
+console.log('CIPHER_KEY  :', cipher_key);
+console.log('CIPHER_IV   :', cipher_iv);
 
-        bot.setDataBase(client);
+const client = new DBClient(db_url);
+client.start()
+    .then(() => {
+        const bot = new Bot(token, 'https://' + url, port);
+
         bot.setName(name);
+        bot.setDataBase(client);
 
-        // keeps server awake
-        setInterval(() => {
-            http.get('http://' + url);
-        }, 10 * 60 * 1000);  // every 10 minutes
+        bot.setCipher(new Cipher({
+            key: cipher_key,
+            iv : cipher_iv
+        }));
+
+        bot.start();
     })
     .catch(error => {
         console.log(error);
     });
+
+// keeps server awake
+setInterval(() => {
+    http.get('http://' + url);
+}, 10 * 60 * 1000);  // every 10 minutes
