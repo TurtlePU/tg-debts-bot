6fd09fe929412c6254bff2e15598572576dcf251
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-02-23 18:27:38 +0300

    optimized types

--- lib/common_types.ts
+++ lib/common_types.ts
@@ -1,47 +1,38 @@
-export interface StatsRow {
-    to_name : string,
-    amount  : number
-};
+import { EventEmitter } from "events";
 
 export interface OfferTemplate {
-    from    : string,
-    amount  : number
+    from:   string,
+    amount: number
 };
 
-export interface TextedOffer extends OfferTemplate {
-    text : string
+export interface Offer extends OfferTemplate {
+    to: string,
+    accept: boolean
 };
 
-export interface OfferOption extends OfferTemplate {
-    accept : boolean
+export interface StatsRow {
+    to_name: string,
+    amount: number
 };
 
-export interface Offer extends OfferOption {
-    to : string
-};
-
-export interface BotPostgreClient {
-    start() : Promise<this>,
-    saveDebt(offer : Offer) : Promise<void>,
-    getStats(name : string) : Promise<StatsRow[]>,
-    setState(chatID : number, state : number) : Promise<void>,
-    checkState(chatID : number, reqState : number) : Promise<boolean>
-};
-
-export interface OfferEncoder {
-    encode(offer : OfferTemplate, accept : boolean) : string,
-    decode(encoded : string) : OfferOption
+export interface BotPostgreClient extends EventEmitter {
+    start(): Promise<this>,
+    saveOffer(id: string, offer: OfferTemplate): Promise<void>,
+    getOffer(id: string): Promise<OfferTemplate>,
+    deleteOffer(id: string): Promise<boolean>,
+    saveDebt(offer: Offer): Promise<void>,
+    getStats(name: string): Promise<StatsRow[]>,
+    setState(chatID: number, state: number): Promise<void>,
+    checkState(chatID: number, reqState: number): Promise<boolean>
 };
 
 export interface BotOptions {
-    token : string,
-    url   : string,
-    port  : number,
-    name  : string,
-    dataBase : BotPostgreClient,
-    cipher : OfferEncoder
+    token: string,
+    port: number,
+    name: string,
+    dataBase: BotPostgreClient
 };
 
 export interface BotType {
-    start() : void
+    start(url: string): void
 };
