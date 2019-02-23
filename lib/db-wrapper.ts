2374a73445fc19f0f89107c01ef9b3d74833b506
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-02-23 23:18:08 +0300

    +code opt, +restart offer

--- lib/db-wrapper.ts
+++ lib/db-wrapper.ts
@@ -33,58 +33,72 @@ export default class MyClient extends Client implements BotPostgreClient {
             create table if not exists offr(
                 id varchar(255) primary key,
                 from_name varchar(32),
-                amount bigint
+                amount bigint,
+                make_time timestamp
             )`
         );
         console.log('\nCreated table of offers if not exists');
         return this;
     }
 
-    async saveOffer(id: string, offer: OfferTemplate): Promise<void> {
-        await this.query(`
-            insert into offr
-            values (
-                '${id}',
-                '${offer.from}',
-                ${offer.amount}
-            )`
-        );
-        console.log('saved offer:', offer, '\non id =', id);
+    expireOffer(id: string, offer: OfferTemplate, expireTime: number): void {
         setTimeout(async () => {
             let deleted = await this.deleteOffer(id);
             if (deleted) {
                 console.log('offer expired:', offer, '\non id =', id);
                 this.emit('expired_offer', id, offer);
             }
-        }, offerExpireTime);
+        }, expireTime);
+    }
+
+    async restartOffers(): Promise<void> {
+        let offers = (await this.query('select * from offr')).rows;
+        let now = Date.now();
+        offers.forEach(offer => {
+            let expires = offer.make_time.getTime() + offerExpireTime;
+            this.expireOffer(
+                offer.id,
+                {
+                    from:   offer.from_name,
+                    amount: offer.amount
+                },
+                Math.max(expires - now, 0)
+            );
+        });
+    }
+
+    async saveOffer(id: string, offer: OfferTemplate): Promise<void> {
+        await this.query(
+            'insert into offr values($1, $2, $3, $4)',
+            [id, offer.from, offer.amount, new Date()]
+        );
+        console.log('saved offer:', offer, '\non id =', id);
+        this.expireOffer(id, offer, offerExpireTime);
     }
 
     async getOffer(id: string): Promise<OfferTemplate> {
         console.log('getting offer on id =', id);
-        let query = await this.query(`
-            select *
-            from offr
-            where id = '${id}'
-            limit 1`
+        let query = await this.query(
+            'select * from offr where id = $1',
+            [id]
         );
         if (query.rowCount === 0)
             throw new Error(`Offer not found. ID = ${id}`);
         let result = query.rows[0];
         console.log('got offer:', result);
         return {
-            from: result.from_name,
+            from:   result.from_name,
             amount: result.amount
         };
     }
 
     async deleteOffer(id: string): Promise<boolean> {
         console.log('trying to delete offer on id =', id);
-        let count = (await this.query(`
-            delete
-            from offr
-            where id = '${id}'`
-        )).rowCount;
-        return count !== 0;
+        let { rowCount } = await this.query(
+            'delete from offr where id = $1',
+            [id]
+        );
+        return rowCount > 0;
     }
 
     async tryMake(name: string): Promise<void> {
@@ -102,6 +116,10 @@ export default class MyClient extends Client implements BotPostgreClient {
             amount = offer.amount,
             to     = offer.to;
         console.log('\nSaving debt...');
+        if (amount === 0) {
+            console.log('0-debt');
+            return;
+        }
         await Promise.all([
             this.insert(from, amount, to),
             this.insert(to, -amount, from)
@@ -114,66 +132,37 @@ export default class MyClient extends Client implements BotPostgreClient {
         to:     string
     ): Promise<void> {
         await this.tryMake(from);
-        let debtExists = await this.unitQuery(`
-            select exists(
-                select *
-                from ${from}
-                where to_name = '${to}'
-            ) as result`
+        let debtQuery = await this.query(
+            `select amount from ${from} where to_name = $1`,
+            [to]
         );
-        if (debtExists) {
-            console.log('\nDebt exists');
-            let oldAmount = parseInt(await this.unitQuery(`
-                select amount
-                as result
-                from ${from}
-                where to_name = '${to}'`
-            ));
-            console.log('\n', this.amounts(amount, oldAmount));
-            if (oldAmount + amount === 0) {
-                console.log('\nDeleting debt...');
-                await this.query(`
-                    delete
-                    from ${from}
-                    where to_name = '${to}'`
-                )
-                let notEmpty = await this.unitQuery(`
-                    select exists(
-                        select *
-                        from ${from}
-                    ) as result`
-                );
-                if (!notEmpty) {
-                    console.log('\nDeleting table...');
-                    await this.query(`drop table ${from}`);
-                }
-            } else {
-                console.log('\nUpdating debt...');
-                await this.query(`
-                    update ${from}
-                    set amount = ${oldAmount + amount}
-                    where to_name = '${to}'`
-                );
-            }
-        } else if (amount != 0) {
-            console.log('\nMaking new debt...');
-            await this.query(`
-                insert into ${from}
-                values ('${to}', ${amount})`
+        amount += debtQuery.rowCount > 0
+                ? debtQuery.rows[0].amount
+                : 0;
+        console.log('new amount:', amount);
+        if (amount !== 0) {
+            await this.query(
+                `insert into ${from} values ($1, $2)`
+                + 'on conflict (to_name) do update set amount = $2',
+                [to, amount]
             );
+            console.log('set or updated debt');
         } else {
-            console.log('\n0-debts are not worth saving');
+            await this.query(
+                `delete from ${from} where to_name = $1`,
+                [to]
+            );
+            console.log('deleted debt');
+            let isEmpty = !(await this.query(
+                `select exists(select * from ${from}) as result`
+            )).rows[0].result;
+            if (isEmpty) {
+                await this.query(`drop table ${name}`);
+                console.log('dropped table');
+            } else {
+                console.log('saved table');
+            }
         }
-    };
-
-    amounts(
-        amount:     number,
-        old_amount: number
-    ): string {
-        return ''
-            + `amount     : ${amount}\n`
-            + `old_amount : ${old_amount}\n`
-            + `sum        : ${amount + old_amount}`;
     };
 
     async getStats(name: string): Promise<StatsRow[]> {
@@ -187,11 +176,10 @@ export default class MyClient extends Client implements BotPostgreClient {
         chatID: number,
         state:  number
     ): Promise<void> {
-        await this.query(`
-            insert into stte
-            values (${chatID}, ${state})
-            on conflict (id) do update
-                set state = ${state}`
+        await this.query(
+            'insert into stte values ($1, $2)'
+            + 'on conflict (id) do update set state = $2',
+            [chatID, state]
         );
     }
 
@@ -199,21 +187,11 @@ export default class MyClient extends Client implements BotPostgreClient {
         chatID:   number,
         reqState: number
     ): Promise<boolean> {
-        let hasState = await this.unitQuery(`
-            select exists(
-                select *
-                from stte
-                where id = ${chatID}
-            ) as result`
+        let query = await this.query(
+            'select state from stte where id = $1',
+            [chatID]
         );
-        if (hasState) {
-            let state = await this.unitQuery(`
-                select state
-                as result
-                from stte
-                where id = ${chatID}`
-            );
-            return new Number(state) === reqState;
-        } else return false;
+        return query.rowCount > 0
+            && query.rows[0].state === reqState;
     }
 };
