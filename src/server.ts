c6542527ef54d93c4363d327e7b3ae223d690b08
Paul Sokolov Jr <sokolov.p64@gmail.com>
2019-05-17 01:36:41 +0300

    + config

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,15 @@ import http from 'http';
 import Bot      from './lib/bot';
 import DBClient from './lib/db-wrapper';
 
-const token  = process.env.TG_BOT_TOKEN || 'your token';
-const url    = process.env.APP_URL      || 'server url';
-const port   = process.env.PORT         || '8080';
-const db_url = process.env.DATABASE_URL || 'PostgreSQL database url';
-const name   = process.env.BOT_NAME     || 'debt_bot'; // goes after @, between 5 & 32 chars long
+// Use .env config if present
+import dotenv from 'dotenv';
+dotenv.config();
+
+const token  = process.env.TG_BOT_TOKEN;            // your token
+const db_url = process.env.DATABASE_URL;            // PostgreSQL connection string
+const url    = process.env.APP_URL;                 // server url
+const port   = process.env.PORT     || '8080';      // port to listen to connections
+const name   = process.env.BOT_NAME || 'debt_bot';  // goes after @, between 5 & 32 chars long
 
 console.log(
     '\nTG_BOT_TOKEN :', token,
@@ -18,19 +22,15 @@ console.log(
 );
 
 async function init() {
-    try {
-        const client = new DBClient(db_url);
-        await client.start();
-        const bot = new Bot({
-            token: token,
-            port:  parseInt(port),
-            name:  name,
-            dataBase: client
-        });
-        await bot.start('https://' + url);
-    } catch(error) {
-        console.log(error);
-    }
+    const client = new DBClient(db_url);
+    await client.start();
+    const bot = new Bot({
+        token: token,
+        port: +port,
+        name:  name,
+        dataBase: client
+    });
+    await bot.start('https://' + url);
 };
 
 init();
